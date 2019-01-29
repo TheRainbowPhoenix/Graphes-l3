32e64bf1a3b14d93d73fe561e724f58e6e2fa613
tfkmaster47 <tfkmaster47@gmail.com>
2019-01-29 19:18:33 +0100

    Cleaning
    Get rid of all the useless functions

--- ffalgo.cpp
+++ ffalgo.cpp
@@ -14,8 +14,6 @@ struct Matrix {
 
 typedef struct Matrix Matrix;
 
-int fordFulkerson(int **, int, int);
-
 Matrix * Matrix_init(int sz) {
   int ** m;
   struct Matrix* matrix = new Matrix;
@@ -61,60 +59,6 @@ void Matrix_print(Matrix *m) {
     }
     std::cout << '\n';
   }
-}
-
-int Matrix_fordFulkerson(Matrix *m) {
-  return fordFulkerson(m->m, 0, (m->sz)-1);
-}
-
-// Returns the maximum flow from s to t in the given graph
-//int fordFulkerson(int graph[V][V], int s, int t)
-int fordFulkerson(int **graph, int s, int t)
-{
-    int u, v;
-
-    // Create a residual graph and fill the residual graph with
-    // given capacities in the original graph as residual capacities
-    // in residual graph
-    int rGraph[V][V]; // Residual graph where rGraph[i][j] indicates
-                     // residual capacity of edge from i to j (if there
-                     // is an edge. If rGraph[i][j] is 0, then there is not)
-    for (u = 0; u < V; u++)
-        for (v = 0; v < V; v++)
-             rGraph[u][v] = graph[u][v];
-
-    int parent[V];  // This array is filled by BFS and to store path
-
-    int max_flow = 0;  // There is no flow initially
-
-    // Augment the flow while tere is path from source to sink
-    while (bfs(rGraph, s, t, parent))
-    {
-        // Find minimum residual capacity of the edges along the
-        // path filled by BFS. Or we can say find the maximum flow
-        // through the path found.
-        int path_flow = INT_MAX;
-        for (v=t; v!=s; v=parent[v])
-        {
-            u = parent[v];
-            path_flow = min(path_flow, rGraph[u][v]);
-        }
-
-        // update residual capacities of the edges and reverse edges
-        // along the path
-        for (v=t; v != s; v=parent[v])
-        {
-            u = parent[v];
-            rGraph[u][v] -= path_flow;
-            rGraph[v][u] += path_flow;
-        }
-
-        // Add path flow to overall flow
-        max_flow += path_flow;
-    }
-
-    // Return the overall flow
-    return max_flow;
 }
 
 int** testLoad() {
